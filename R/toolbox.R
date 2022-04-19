f7a50a26c65f8f1c76fd0f962d9b053442d40af0
ahreed304 <ahreed304@gmail.com>
2022-04-19 11:16:49 -0500

    commented out everything but one function

--- R/toolbox.R
+++ R/toolbox.R
@@ -4,30 +4,30 @@ library(fortunes)
 library(data.table)
 library(tidyverse)
 
-mkdir <- function(path) {
-  if (file.exists(path)) {
-    # "Directory/file exists at {path}.  Cannot create directory." |> glue() |> warning()
-  }
-  else{
-    'Creating new directory {path}' |> glue() |> print()
-    dir.create(path)
-  }
-  path
-}
-
-'%!in%' <- function(x,y)!('%in%'(x,y))
-
-get_datetime_string <- function() {
-  now(tzone = "US/Central") %>%
-    as.character() %>%
-    gsub(" ", "_", .) %>%
-    gsub(":", ".", .)
-}
-
-save_object <- function(obj, dir, prefix = "") {
-  filename <- '{dir}/{prefix}_{get_datetime_string()}.rds' |> glue()
-  obj |> saveRDS(filename)
-}
+# mkdir <- function(path) {
+#   if (file.exists(path)) {
+#     # "Directory/file exists at {path}.  Cannot create directory." |> glue() |> warning()
+#   }
+#   else{
+#     'Creating new directory {path}' |> glue() |> print()
+#     dir.create(path)
+#   }
+#   path
+# }
+#
+# '%!in%' <- function(x,y)!('%in%'(x,y))
+#
+# get_datetime_string <- function() {
+#   now(tzone = "US/Central") %>%
+#     as.character() %>%
+#     gsub(" ", "_", .) %>%
+#     gsub(":", ".", .)
+# }
+#
+# save_object <- function(obj, dir, prefix = "") {
+#   filename <- '{dir}/{prefix}_{get_datetime_string()}.rds' |> glue()
+#   obj |> saveRDS(filename)
+# }
 
 
 # merge_dfs <- function(files, ids) {
@@ -59,34 +59,34 @@ merge_dfs <- function(files, ids) {
   }) |> bind_rows()
 }
 
-merge_pdfs <- function(dir) {
-  qpdf::pdf_combine(
-    input = dir |> list.files(full.names = TRUE) |> as.character(),
-    output = "{output_root}/!plots_combined.pdf" |> glue()
-  )
-}
-
-# samples <- dirs$cellranger$tcr |> list.files()
-# files <- "{dirs$cellranger$tcr}/{samples}/outs/filtered_contig_annotations.csv" |> glue()
-# foo <- merge_dfs(files, samples)
-cowtime <- function(expr, name = NA) {
-  s_time <-  now(tzone = "US/Central")
-  msg <- s_time |> as.character()
-  # msg <- s_time |> as.character()
-  if (!is.na(name)) {
-    msg <- glue("\nstarting \"{name}\"\n{msg}")
-  }
-  say(msg, "chicken")
-  expr
-  e_time <- now(tzone = "US/Central")
-  delta_time <- (e_time - s_time) %>% as.duration %>% as.character
-  msg <- glue("Execution time: \n{delta_time}")
-  if (!is.na(name)) {
-    msg <- glue("Execution time for \"{name}\": \n{delta_time}")
-  }
-  say(msg, "cow")
-}
-
-distraction <- function() {
-  say("fortune","random")
-}
+# merge_pdfs <- function(dir) {
+#   qpdf::pdf_combine(
+#     input = dir |> list.files(full.names = TRUE) |> as.character(),
+#     output = "{output_root}/!plots_combined.pdf" |> glue()
+#   )
+# }
+#
+# # samples <- dirs$cellranger$tcr |> list.files()
+# # files <- "{dirs$cellranger$tcr}/{samples}/outs/filtered_contig_annotations.csv" |> glue()
+# # foo <- merge_dfs(files, samples)
+# cowtime <- function(expr, name = NA) {
+#   s_time <-  now(tzone = "US/Central")
+#   msg <- s_time |> as.character()
+#   # msg <- s_time |> as.character()
+#   if (!is.na(name)) {
+#     msg <- glue("\nstarting \"{name}\"\n{msg}")
+#   }
+#   say(msg, "chicken")
+#   expr
+#   e_time <- now(tzone = "US/Central")
+#   delta_time <- (e_time - s_time) %>% as.duration %>% as.character
+#   msg <- glue("Execution time: \n{delta_time}")
+#   if (!is.na(name)) {
+#     msg <- glue("Execution time for \"{name}\": \n{delta_time}")
+#   }
+#   say(msg, "cow")
+# }
+#
+# distraction <- function() {
+#   say("fortune","random")
+# }
